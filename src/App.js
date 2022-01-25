b9ba210b0ad7936228f002e72eb83360ec12be65
vicentefiorito <vicentefiorito@gmail.com>
2022-01-25 17:40:37 -0500

    added the ExpenseItem component into App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
      <h2>Let's get started</h2>
-     <p>This is also visible</p>
+    <ExpenseItem></ExpenseItem>
     </div>
   );
+  
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,3 +4,5 @@ import reactDom from "react-dom";
 function ExpenseItem() {
     return <h2>Expense New Item!</h2>
 }
+
+export default ExpenseItem;
