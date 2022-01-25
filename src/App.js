ed994e30c01c22641280892e1358b881aa9da91b
vicentefiorito <vicentefiorito@gmail.com>
2022-01-25 18:56:03 -0500

    Added the ExpensesDisplay component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import ExpensesDisplay from './components/ExpensesDisplay';
 
 function App() {
     //adds the expense items as an array
@@ -29,31 +29,7 @@ function App() {
   return (
     <div className="App">
      <h2>Let's get started</h2>
-      <ExpenseItem 
-        title={expenseItems[0].title} 
-        amount={expenseItems[0].amount} 
-        date={expenseItems[1].date}
-      />
-      <ExpenseItem 
-        title={expenseItems[1].title} 
-        amount={expenseItems[1].amount} 
-        date={expenseItems[1].date}
-      ></ExpenseItem>
-      <ExpenseItem 
-        title={expenseItems[2].title} 
-        amount={expenseItems[2].amount} 
-        date={expenseItems[2].date}
-      />
-      <ExpenseItem 
-        title={expenseItems[3].title} 
-        amount={expenseItems[3].amount} 
-        date={expenseItems[3].date}
-      />
-      <ExpenseItem 
-        title={expenseItems[4].title} 
-        amount={expenseItems[4].amount} 
-        date={expenseItems[4].date}
-     />
+      <ExpensesDisplay expenses={expenseItems}/>
     </div>
   );
   
